9cf02bc677eaf8a23c947b9ec1557656a413ded9
Rayan <rayanalhassan2000@gmail.com>
2024-03-01 09:30:07 +0200

    maket the origin in corse to localhost 3000 and link from render for back

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:3000, https://skinwisdomhub-server.onrender.com",
     credentials: true,
     optionsSuccessStatus: 200,
   })
